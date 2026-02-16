6824e5e313611a0fabcebedb31060702eef87231
eloi bocomehounou <giovannieloi7@gmail.com>
2026-02-16 13:42:45 -0500

    catching up and pushing for backup

--- src/services/todo.services.js
+++ src/services/todo.services.js
@@ -1,17 +1,17 @@
+// WILL NEVER HAVE ANYTHING RELATING TO HTTP CALLS OR RESPONSES
 
+import * as ToDoModel from "../models/todo.models.js"
 
-import ToDoModel from "../models/todo.models.js"
-
-function getTodosService(){
-    return ToDoModel.getAllTodos();
+export async function getTodosService(){
+    return await ToDoModel.getAllTodos();
 }
 
-function createTodoService(task){
+export async function createTodoService(task){
     if(!task || typeof task !=="string" || task.trim()===""){
         // return res.status(400).json({error:"task is required. You should provide non-empty string"});
         throw new error("Invalid task")
     }
-    return ToDoModel.createTodo(task);
+    return await ToDoModel.createTodo(task);
 }
 
 function toggleTodoByIdService(id){
@@ -28,8 +28,8 @@ function deleteTodoByIdService(id){
 
 
 export {
-    getTodosService,
-    createTodoService,
+    
+    
     toggleTodoByIdService,
     deleteTodoByIdService
 
